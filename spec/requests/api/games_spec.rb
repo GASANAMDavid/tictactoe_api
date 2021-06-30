0411253c68b591a9b98d6bbb3d6527b2f35fa7ec
David Gasana Manzi <gmdavid59@gmail.com>
2021-06-30 15:51:02 +0200

    domenting the api requests

--- spec/requests/api/games_spec.rb
+++ spec/requests/api/games_spec.rb
@@ -16,48 +16,36 @@ RSpec.describe 'api/games', type: :request do
         },
         required: %w[player_name game_mode language board_size symbol]
       }
-      response '201', 'blog created' do
+      response '201', 'game created' do
         let(:game) { { player_name: 'Manzi', symbol: 'X', game_mode: 2, language: 'en', board_size: 3 } }
         run_test!
       end
 
-      response '204', 'invalid request' do
-        let(:game) { { player_name: 'David', game_mode: 2, language: 'en', board_size: 3 } }
-        run_test!
-      end
+      # response '422', 'invalid request' do
+      #   let(:game) { { player_name: 'David', game_mode: 2, language: 'en', board_size: 3 } }
+      #   run_test!
+      # end
     end
   end
 
   # post '/games', params: game_params, as: :json
 
   describe '#play' do
-    let(:game_params) do
-      {
-        "language": 'fr',
-        "player_name": 'David',
-        "game_mode": '2',
-        "board_size": '3',
-        "symbol": 'X'
-      }
-    end
-    before do
-      post '/games', params: game_params, as: :json
-      id = response.parsed_body['id']
-    end
-    path "/games/1/play" do
+    path '/games/{id}/play' do
       put 'Plays the game registered on a record with id 1' do
         produces 'application/json'
         consumes 'application/json'
+        parameter name: :id, in: :path, type: :string
         parameter name: :move, in: :body, schema: {
           type: :object,
           properties: {
-            id: { type: :string, in: :path },
-            move: { type: :string, in: :body }
+            move: { type: :string }
           },
           required: %w[id move]
         }
-        response '201', 'move applied' do
+        response '200', 'move applied' do
           let(:move) { { move: 1 } }
+          let(:id) { create(:game).id }
           run_test!
         end
       end
